5d37196cf8c4710ffaef67f666065ca045a8b94c
Thyceekay <108959771+Thyceekay@users.noreply.github.com>
2023-06-22 18:04:04 +0100

    Add files via upload
    Updated changes to test_run

--- test_run/bin/test_run.dart
+++ test_run/bin/test_run.dart
@@ -3,35 +3,12 @@ import 'dart:io';
 void main(List<String> arguments) {
   fileConverter();
 }
+void fileConverter(){
+  var inputLines = File('input.txt').readAsLinesSync();
+  var excludeLines = File('exclude.txt').readAsLinesSync();
 
-dynamic fileConverter() {
-  var outputTxt = File('output.txt');
-  File('file.txt').readAsString().then((String contents) {
-    print(contents);
-  });
+  var outputLines = inputLines.where((line) => line.trim().isNotEmpty && !excludeLines.contains(line.trim()));
 
-  outputTxt.writeAsStringSync("\nFinal try", mode: FileMode.append);
-
-  outputTxt.readAsLinesSync();
-
-  if (outputTxt != File('input.txt')) {
-    {
-      return null;
-    }
-  }
-  if (outputTxt.lengthSync() == 0) {
-    return null;
-  }
-  if (File('input.txt').readAsLinesSync() == File('exclude.txt').readAsLinesSync()) {
-    return content('');
-  } else if (File('input.txt').lengthSync() > File('exclude.txt').lengthSync()) {
-    String contain = File('input.txt') as String;
-    outputTxt = contain as File;
-    return outputTxt;
-  }
-}
-
-String content(String let) {
-  let = File('output.txt').readAsLinesSync() as String;
-  return let;
+  var outputTxt = outputLines.join('\n');
+  File('output.txt').writeAsStringSync(outputTxt);
 }
