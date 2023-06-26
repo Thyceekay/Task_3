5e3728caaaa9de6e12180b85075fcfb167772984
Thyceekay <108959771+Thyceekay@users.noreply.github.com>
2023-06-26 14:30:37 +0100

    Update test_run.dart
    An expanded logic behind my code with broader explanations on my decisions

--- test_run/bin/test_run.dart
+++ test_run/bin/test_run.dart
@@ -4,11 +4,19 @@ void main(List<String> arguments) {
   fileConverter();
 }
 void fileConverter(){
+
+// Read input.txt and exclude.txt files
+
   var inputLines = File('input.txt').readAsLinesSync();
   var excludeLines = File('exclude.txt').readAsLinesSync();
 
+// Filter lines that are in input.txt but not in exclude.txt 
+// Remove empty lines by trimming the spaces at the beginnings and ends of lines
+// Ensuring that inputLines is not empty
+ 
   var outputLines = inputLines.where((line) => line.trim().isNotEmpty && !excludeLines.contains(line.trim()));
+  
+// Write outputLines to output.txt file
 
-  var outputTxt = outputLines.join('\n');
-  File('output.txt').writeAsStringSync(outputTxt);
+  var outputTxt = outputLines.join('\n');   File('output.txt').writeAsStringSync(outputTxt);
 }
