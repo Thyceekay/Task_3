4529e4c117ee9f26317dde363acac972cdde9b6d
Thyceekay <108959771+Thyceekay@users.noreply.github.com>
2023-07-03 17:12:04 +0100

    Update test_run.dart
    Updated requirements with comments

--- test_run/bin/test_run.dart
+++ test_run/bin/test_run.dart
@@ -1,22 +1,36 @@
 import 'dart:io';
 
-void main(List<String> arguments) {
-  fileConverter();
-}
-void fileConverter(){
-
-// Read input.txt and exclude.txt files
-
+void f1leConverter() {
+  // Read input.txt and exclude.txt files
   var inputLines = File('input.txt').readAsLinesSync();
   var excludeLines = File('exclude.txt').readAsLinesSync();
 
-// Filter lines that are in input.txt but not in exclude.txt 
-// Remove empty lines by trimming the spaces at the beginnings and ends of lines
-// Ensuring that inputLines is not empty
- 
-  var outputLines = inputLines.where((line) => line.trim().isNotEmpty && !excludeLines.contains(line.trim()));
-  
-// Write outputLines to output.txt file
+  // List to store output lines
+  var outputLines = <String>[];
 
-  var outputTxt = outputLines.join('\n');   File('output.txt').writeAsStringSync(outputTxt);
+  // Iterate over each line in inputLines
+  for (var line in inputLines) {
+    // Separate the line before trimming
+    var separatedLine = separateLine(line);
+
+    // Check if the separated line is non-empty and not present in excludeLines
+    if (separatedLine.trim().isNotEmpty && !excludeLines.contains(separatedLine.trim())) {
+      // Add the separated line to the outputLines list
+      outputLines.add(separatedLine);
+    }
+  }
+
+  // Join outputLines with newline separator
+  var outputText = outputLines.join('\n');
+
+  // Write outputText to output.txt file
+  File('output.txt').writeAsStringSync(outputText);
 }
+
+// Function to separate the line by spaces
+String separateLine(String line) {
+  var separatedParts = line.split(' ');
+  var separatedLine = separatedParts.join(' ');
+  return separatedLine;
+}
+
